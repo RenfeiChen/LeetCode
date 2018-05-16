8222b37af36ceaca9490ac68a1d8a383b24deeb4
Renfei Chen <chenrf96@uw.edu>
2018-05-16 17:39:34 +0800

    LC390

---
+++ Elimination Game(LC390).java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lastRemaining(int n) {
+        int head = 1, step = 1;
+        boolean left = true;
+        while (n > 1) {
+            if (left || n % 2 == 1) {
+                head += step;
+            }
+            step *= 2;
+            left = !left;
+            n /= 2;
+        }
+        return head;
+    }
+}
