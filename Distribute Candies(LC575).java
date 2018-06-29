573b6159360ad01c43ee9cb4bea95bfd395c0d43
Renfei Chen <chenrf96@uw.edu>
2018-06-29 14:36:43 +0800

    LC575

---
+++ Distribute Candies(LC575).java
@@ -0,0 +1,9 @@
+class Solution {
+    public int distributeCandies(int[] candies) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : candies) {
+            set.add(num);
+        }
+        return Math.min(candies.length / 2, set.size());
+    }
+}
